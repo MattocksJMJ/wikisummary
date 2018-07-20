d7c768e990fbdc1f32517765c1fbe5fbbc7296f6
Jamie Mattocks <40267073+MattocksJMJ@users.noreply.github.com>
2018-07-20 11:39:54 +0100

    Update wikiscraper.py
    Some wikipedia articles store the first <b> tag of the <p> tag in an <i> tag, so I implemented an if statement to check for the <i> tags contents being the same as the <b> tag. In short checks to see if the title is the same as the text if so grab the parent of the text and continue. Also some articles cause issues when trying to write the text to a text file, so I implemented a try function to check if there is a UnicodeEncodeError if so encode the text to utf-8.

--- wikiscraper.py
+++ wikiscraper.py
@@ -1,29 +1,56 @@
 import webbrowser, requests, time
 from bs4 import BeautifulSoup
 
-
-
+# Ask user to input a wikipedia page 
 q   = print('Please input the wikipedia page you would like summarised: ')
 url = input()
+# Open the page for the user to view in their browser
 res = webbrowser.open(url)
 
+# Open the same page with beautiful soup to be searched
 resSoup  = requests.get(url)
 soup     = BeautifulSoup(resSoup.text,'html.parser')
 
+# Select the class mw-parser-output with a <p> tag and a <b> tag
 synopsis     = soup.select('.mw-parser-output p b')
 synopsisText = synopsis[0].getText()
 
-# print(synopsisText)
-
+# Get the parent of the current b tag and convert to text
 getParent = synopsis[0].parent
 end       = getParent.getText()
 
-print()
+# If the bold text (title) and body of text are the same then get the parent of the current tag
+if synopsisText == end:
+    end = getParent.parent
+    end1= end.getText()
+    print()
+    print(synopsisText)
+    print()
+    print(end1)
+    # Create a text file named the title in the wikipedia folder
+    path = 'wikipedia/'
+    file = open(path+synopsisText+".txt", 'w')
+    # Try to write the text into the file
+    try:
+        file.write(str(end1))
+    # If the attempt returns a UnicodeEncodeError then encode in utf-8
+    except (UnicodeEncodeError):
+        file.write(str(end1.encode('utf-8')))
+    file.close()
+else:
+    print()
+    print(synopsisText)
+    print()
+    print(end)
+    # Create a text file named the title in the wikipedia folder
+    path = 'wikipedia/'
+    file = open(path+synopsisText+".txt", 'w')
+    # Try to write the text into the file
+    try:
+        file.write(str(end))
+    # If the attempt returns a UnicodeEncodeError then encode in utf-8
+    except (UnicodeEncodeError):
+            file.write(str(end.encode('utf-8')))
+    file.close()
 
-print(end)
-
-path = 'wikipedia/'
-
-file = open(path+synopsisText+".txt", 'w')
-file.write(end)
-file.close()
+    print('Thank you for saving the top paragraph of the wikipedia article: '+synopsisText)
